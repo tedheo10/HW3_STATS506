32dcd458f19d9c78266e076047b12d946e928a60
mymy <mymy@mymyui-MacBookPro.local>
2024-10-04 14:15:38 -0400

    complete_v1

--- hw3-3.R
+++ hw3-3.R
@@ -1,3 +1,55 @@
 
 # Problem 3 - US Records 
 
+# a. the proportion of email addresses with TLD “.com”
+# the proportion : 0.732
+
+us500 <- read.csv("us-500.csv")
+
+email_us <- us500$email
+total <- length(email_us) # total number of email addresses
+
+com <- email_us[grepl("@.+\\.com",email_us)]
+sub <- length(com) # the number of email addresses with TLD ".com"
+
+sub/total # the proportion of email addresses with TLD “.com”
+
+# b. the proportion of email addresses with at least one non alphanumeric character
+# the proportion : 0.506  
+
+non <- email_us[grepl("[^a-zA-Z0-9].*@.+\\..+",email_us)]
+sub2 <- length(non) # the number of email addresses with a non alphanumeric character
+
+sub2/total # the proportion of email addresses with a non alphanumeric character
+
+# c. the top 5 most common area codes amongst all phone numbers
+# the top 5 : 973, 212, 215, 410, 201
+
+phone1_us <- us500$phone1
+phone2_us <- us500$phone2
+area1 <- substr(phone1_us, 1, 3) # the area code from phone1
+area2 <- substr(phone2_us, 1, 3) # the area code from phone2
+sum(area1 != area2) # check whether the two area codes differ or not 
+area_table <- table(area1)
+area_sorted <- sort(area_table, decreasing = TRUE)
+names(head(area_sorted, 5)) # the 5 most common area codes amongt all phone numbers 
+
+# d. a histogram of the log of the apartment numbers for all addresses
+  
+address <- us500$address
+apt_num <- regmatches(address, regexpr("\\d+$", address)) # any number at the end of the an address 
+apt_num <- as.numeric(apt_num)
+hist(log(apt_num))
+
+
+# e. Examination whether the apartment numbers appear to follow Benford’s law
+# the chi-squared test : p-value = 1.158e-08 
+# We can reject the hypothesis that the apartment numbers follow Benford’s law. 
+
+chr <- as.character(apt_num) 
+digit <- substr(chr,1,1) # the leading digit of the apartment numbers
+digit <- as.numeric(digit)
+
+count <- table(digit) # the number of the leading digit of the apartment numbers
+ben_p <- log10(1+(1/1:9)) # the probability of real numbers satifying Benford's law 
+chisq.test(count, p = ben_p) # test for compliance with Benford's law
--- hw3.R
+++ hw3.R
@@ -64,15 +64,57 @@ table_glasses <- stargazer(df_glasses, type = "text", summary = FALSE,
 
 # c. Fit three logistic regression models predicting whether a respondent wears glasses/contact lenses for distance vision
 
-# 1) Predictors: age
-lm1 <- lm(VIQ220~RIDAGEYR, data=merge_data)
-summary(lm1)
+merge_data$VIQ220_BI <- merge_data$VIQ220 == 1 # Binomial data for VIQ220
 
-# 2) Predictors: age, race, gender
-lm2 <- lm(VIQ220~RIDAGEYR+RIDRETH1+RIAGENDR, data=merge_data)
-summary(lm2)
+# 1) the 1st logistic regression with age as a predictor
+ 
+glm1 <- glm(VIQ220_BI ~ RIDAGEYR, data = merge_data, family = binomial) 
+odd_1 <- exp(coef(glm1)) # the oods ratios 
+num1 <- nobs(glm1) # sample size
+pseudo_r1 <- 1 - (glm1$deviance / glm1$null.deviance) # pseudo-R^2
+aic1 <- AIC(glm1) # AIC value
 
-# 3) Predictors: age, race, gender, Poverty Income ratio
-lm3 <- lm(VIQ220~RIDAGEYR+RIDRETH1+RIAGENDR+INDFMPIR, data=merge_data)
-summary(lm3)
+# 2) the 2nd logistic regression with age, race and gender as predictors  
+glm2 <- glm(VIQ220_BI ~ RIDAGEYR+RIDRETH1+RIAGENDR, data=merge_data, family = binomial)
+odd_2 <- exp(coef(glm2)) # the oods ratios 
+num2 <- nobs(glm2) # sample size
+pseudo_r2 <- 1 - (glm2$deviance / glm2$null.deviance) # pseudo-R^2
+aic2 <- AIC(glm2) # AIC value
 
+# 3) the 3rd logistic regression with age, race, gender and poverty income ratio as predictors 
+glm3 <- glm(VIQ220_BI ~ RIDAGEYR+RIDRETH1+RIAGENDR+INDFMPIR, data=merge_data, family = binomial)
+odd_3 <- exp(coef(glm3)) # the oods ratios 
+num3 <- nobs(glm3) # sample size
+pseudo_r3 <- 1 - (glm2$deviance / glm2$null.deviance) # pseudo-R^2
+aic3 <- AIC(glm3) # AIC value
+
+# Create a summary table of the three logistic models 
+
+summary_table <- data.frame(
+  Model_1 = c(odd_1[2], NA, NA, NA, num1, pseudo_r1, aic1),
+  Model_2 = c(odd_2[2], odd_2[3], odd_2[4], NA, num2, pseudo_r2, aic2),
+  Model_3 = c(odd_3[2], odd_3[3], odd_3[4], odd_3[5], num3, pseudo_r3, aic3),
+  row.names = c("Odd_Ratio_Age", "Odd_Ratio_Race", "Odd_Ratio_Gender", "Odd_Ratio_PIR", "Sample_Size", "Pseudo_R2", "AIC")
+)
+
+stargazer(summary_table, type = "text", summary = FALSE, title = "Summary of the three Logistic Regression Models")
+
+
+#d. From glm3, test and interpretation 
+
+# 1) Test whether the odds of men and women being wears of glasess/contact lenses for distance vision differs. 
+
+# the p-value of t-test for the coefficient for the gender variable(RIAGENDR) is very small(p-value = 1.153700e-21. This means that the odds between men and women are significantly different. 
+
+summary(glm3)$coeff["RIAGENDR", ] # the test of the coefficient for RIAGENDR 
+
+# 2) Test whether the proportion of wearers of glasses/contact lenses for distance vision differs between men and women. 
+
+# the p-value of chi-squared test for the proportion between males and females is very small(p-value < 2e-16). This means that the proportions between men and women are significantly different. 
+
+pp_table <- table(merge_data$RIAGENDR, merge_data$VIQ220_BI)
+pp_table # "1" means male and "2" means female
+pp_table[1,2]/sum(pp_table[1,]) # the proportion of male 
+pp_table[2,2]/sum(pp_table[2,]) # the proportion of female
+chi_test <- chisq.test(pp_table) # the chi-squared test for the proportion difference
+chi_test 
